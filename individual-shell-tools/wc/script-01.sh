abc4166983ab001248635a8177b5c47cb6ead061
Trang <dothuytrang2312@gmail.com>
2026-03-30 19:57:40 +0200

    Complete wc exercises

--- individual-shell-tools/wc/script-01.sh
+++ individual-shell-tools/wc/script-01.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 # TODO: Write a command to output the number of words in the file helper-files/helper-3.txt.
 # The output should include the number 19. The output should not include the number 92.
+
+wc -w ../helper-files/helper-3.txt
--- individual-shell-tools/wc/script-02.sh
+++ individual-shell-tools/wc/script-02.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 
 # TODO: Write a command to output the number of lines in the file helper-files/helper-3.txt.
 # The output should include the number 3. The output should not include the number 19.
+wc -l ../helper-files/helper-3.txt 
