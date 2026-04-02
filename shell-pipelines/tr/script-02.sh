ab595a72d6fe489b498fac3544ab83832a348b36
Trang <dothuytrang2312@gmail.com>
2026-04-02 03:12:42 +0200

    Complete tr

--- shell-pipelines/tr/script-02.sh
+++ shell-pipelines/tr/script-02.sh
@@ -7,3 +7,4 @@ set -euo pipefail
 # so every Y should be a Z, and every Z should be a Y!
 #
 # TODO: Write a command to output the contents of text.txt with every Y and Z swapped (both upper and lower case).
+tr 'yYzZ' 'zZyY' < text.txt
