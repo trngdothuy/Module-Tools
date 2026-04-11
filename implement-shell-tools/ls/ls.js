928a986870fbbfe168ab780084b03c5cd86acbf1
Trang <dothuytrang2312@gmail.com>
2026-04-12 00:31:36 +0200

    Finish ls.js implementation

---
+++ implement-shell-tools/ls/ls.js
@@ -0,0 +1,29 @@
+import { program } from "commander";
+import { promises as fs } from "node:fs";
+
+program
+    .name("ls")
+    .description("List all files name")
+    .option("-1", "A new line for each file")
+    .option("-a", "Show hidden files")
+    .argument("[path]", "The directory to list", ".");
+
+program.parse();
+
+let path = "";
+program.args.length == 0 ? path = '.' : path = program.args[0];
+let flags = program.opts()
+
+try {
+    let files = await fs.readdir(path);
+    if (!flags["a"]) {
+        files = files.filter(file => !file.startsWith("."))
+    }
+    if (flags["1"]) {
+        console.log(files.join("\n"));
+    } else {
+        console.log(files.join("       "));
+    }
+} catch (err) {
+    console.error(`ls - ${path}: ${err.message}`)
+}
